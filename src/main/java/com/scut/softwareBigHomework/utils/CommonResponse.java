14c975074181fc1c2827523561eb202a28773418
yinziming02 <1624167381@qq.com>
2025-06-03 19:57:21 +0800

    fix: 补充注解

--- src/main/java/com/scut/softwareBigHomework/utils/CommonResponse.java
+++ src/main/java/com/scut/softwareBigHomework/utils/CommonResponse.java
@@ -1,8 +1,12 @@
 package com.scut.softwareBigHomework.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommonResponse {
     private int code;
     private String message;
