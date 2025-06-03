6e27ba28a21876a60487bca99aefaa4b3f7a1211
yinziming02 <1624167381@qq.com>
2025-06-03 11:17:45 +0800

    init

---
+++ src/main/java/com/scut/softwareBigHomework/SoftwareBigHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.scut.softwareBigHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareBigHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareBigHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scut/softwareBigHomework/SoftwareBigHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scut.softwareBigHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareBigHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
