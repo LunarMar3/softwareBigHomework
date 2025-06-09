97e7d9928b0ff9804d94701400c000856617ab23
yinziming02 <1624167381@qq.com>
2025-06-09 19:36:34 +0800

    feat: 查询工单日志

--- src/main/java/com/scut/softwareBigHomework/controller/WorkOrderLogController.java
+++ src/main/java/com/scut/softwareBigHomework/controller/WorkOrderLogController.java
@@ -1,8 +1,11 @@
 package com.scut.softwareBigHomework.controller;
 
 import com.scut.softwareBigHomework.service.WorkOrderLogService;
+import com.scut.softwareBigHomework.utils.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,9 @@ public class WorkOrderLogController {
 
     @Autowired
     private WorkOrderLogService workOrderLogService;
+
+    @GetMapping("/getAll")
+    public CommonResponse getAllWorkOrderLogs(@RequestParam("index")Integer index){
+        return workOrderLogService.getAllWorkOrderLogs(index);
+    }
 }
